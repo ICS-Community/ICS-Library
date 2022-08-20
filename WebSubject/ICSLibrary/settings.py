762526ae9c376ae0a1afadc12436f75bffcef33c
HYH! <huangyinhaow@outlook.com>
2022-08-20 18:08:47 +0800

    add css

--- WebSubject/ICSLibrary/settings.py
+++ WebSubject/ICSLibrary/settings.py
@@ -83,14 +83,18 @@ WSGI_APPLICATION = 'ICSLibrary.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ICS-Library',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'ICS-Library',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
